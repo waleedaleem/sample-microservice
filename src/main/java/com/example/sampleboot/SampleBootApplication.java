a7c441c1af121328e3b2482b1261e543a2eb4178
Walid Moustafa <waleedaleem@hotmail.com>
2018-07-23 15:30:13 +1000

    initial commit

---
+++ src/main/java/com/example/sampleboot/SampleBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.sampleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleBootApplication.class, args);
+    }
+}
